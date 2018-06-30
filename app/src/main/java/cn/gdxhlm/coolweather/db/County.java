20e66569bb0df4776ade80725356d7ecd241d2f2
gdxhlm <434483420@qq.com>
2018-06-30 11:21:37 +0800

    加入创建数据库和表的各项配置

---
+++ app/src/main/java/cn/gdxhlm/coolweather/db/County.java
@@ -0,0 +1,30 @@
+package cn.gdxhlm.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+public class County extends DataSupport {
+    private int id;
+    private String countyName;
+    private int countyCode;
+
+    public int getId(){
+        return id;
+    }
+    public void setId(int id){
+        this.id=id;
+    }
+
+    public String getCountyName(){
+        return countyName;
+    }
+    public void setCountyName(String countyName){
+        this.countyName=countyName;
+    }
+
+    public int getCountyCode(){
+        return  countyCode;
+    }
+    public void setCountyCode(int countyCode){
+        this.countyCode=countyCode;
+    }
+}
